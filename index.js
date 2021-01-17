7b2c56af165fa9ede56d6d50be7b9f4794a4141e
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-17 16:51:26 +0300

    test6

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import favicon from 'serve-favicon'
 import config from 'config'
-import path from 'path'
 
-const app = express()
+
+import path from 'path'
 const PORT = process.env.PORT && config.get("PORT")
 const __dirname = path.resolve()
 
 
-app.use(express.json())
+const app = express()
+app.use(favicon(__dirname + '/build/favicon.ico'))
+
+
+app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
 
@@ -19,6 +24,8 @@ app.get('/ping', (req, res) => {
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
+
 
 
 app.listen(PORT, () => { console.log(`Server has been starten on PORT=${PORT}...`) })
