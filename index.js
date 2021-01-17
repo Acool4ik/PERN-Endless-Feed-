bc0641d740f12ce199dfda1dbebc36cd169bd619
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-17 17:17:52 +0300

    test10

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 import express from 'express'
 import favicon from 'serve-favicon'
 import config from 'config'
-
-
 import path from 'path'
-const PORT = process.env.PORT && config.get("PORT")
+
+
+const PORT = process.env.PORT || config.get("PORT")
 const __dirname = path.resolve()
 
 
 const app = express()
-// app.use(favicon(__dirname + '/build/favicon.ico'))
 
-
-// app.use(express.static(__dirname))
-// app.use(express.static(path.join(__dirname, 'build')))
-
-
-app.get('/ping', (req, res) => {
-    return res.json({payload: 'pong'})
-})
 
 app.get('/', (req, res) => {
-    return res.json({payload: 'ok'})
+    res.json({payload: 'home page'})
 })
 
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
-
+app.get('/about', (req, res) => {
+    res.json({payload: 'about page'})
+})
 
 
 
