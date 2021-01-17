8fc7e47dd85f09886acac725be27cbb139b754f0
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-17 17:40:57 +0300

    latist

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ const __dirname = path.resolve()
 
 const app = express()
 app.use(express.json())
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.get('/frontend*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 
