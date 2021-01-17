4d9a9e7f36b9ac685b7ca4a973a7fe682f9172e0
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-17 17:02:51 +0300

    test7

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ const __dirname = path.resolve()
 
 
 const app = express()
-app.use(favicon(__dirname + '/build/favicon.ico'))
+// app.use(favicon(__dirname + '/build/favicon.ico'))
 
 
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(__dirname))
+// app.use(express.static(path.join(__dirname, 'build')))
 
 
 app.get('/ping', (req, res) => {
     return res.json({payload: 'pong'})
 })
 
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get('/', (req, res) => {
+    return res.json({payload: 'ok'})
 })
+
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
 
 
 
