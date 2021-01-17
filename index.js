78ec805c82ab7c6f3ae749dc29e25567971eaa04
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-17 17:34:11 +0300

    last

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const __dirname = path.resolve()
 
 const app = express()
 app.use(express.json())
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 
